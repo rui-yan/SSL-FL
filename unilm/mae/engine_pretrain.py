f3516c8d9b96fa2d5899f43f8ca4e39cfc852ac4
Rui Yan <yanrui0498@gmail.com>
2022-02-26 13:24:58 -0800

    add mae

---
+++ unilm/mae/engine_pretrain.py
@@ -0,0 +1,104 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# DeiT: https://github.com/facebookresearch/deit
+# BEiT: https://github.com/microsoft/unilm/tree/master/beit
+# --------------------------------------------------------
+import math
+import sys
+from typing import Iterable
+
+import torch
+
+import util.misc as misc
+import util.lr_sched as lr_sched
+
+
+def train_one_epoch(model: torch.nn.Module,
+                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
+                    device: torch.device, epoch: int, loss_scaler,
+                    log_writer=None,
+                    args=None):
+    model.train(True)
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 20
+    
+    accum_iter = args.accum_iter
+
+    optimizer.zero_grad()
+
+    if log_writer is not None:
+        print('log_dir: {}'.format(log_writer.log_dir))
+
+    for data_iter_step, (samples, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+
+        # we use a per iteration (instead of per epoch) lr scheduler
+        if data_iter_step % accum_iter == 0:
+            lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+
+        samples = samples.to(device, non_blocking=True)
+
+        with torch.cuda.amp.autocast():
+            loss, _, _ = model(samples, mask_ratio=args.mask_ratio)
+
+        loss_value = loss.item()
+        
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)
+
+        loss /= accum_iter
+        loss_scaler(loss, optimizer, parameters=model.parameters(),
+                    update_grad=(data_iter_step + 1) % accum_iter == 0)
+        if (data_iter_step + 1) % accum_iter == 0:
+            optimizer.zero_grad()
+
+        torch.cuda.synchronize()
+
+        metric_logger.update(loss=loss_value)
+
+        lr = optimizer.param_groups[0]["lr"]
+        metric_logger.update(lr=lr)
+
+        loss_value_reduce = misc.all_reduce_mean(loss_value)
+        # if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
+        #     """ We use epoch_1000x as the x-axis in tensorboard.
+        #     This calibrates different curves when batch size changes.
+        #     """
+        #     epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
+        #     log_writer.add_scalar('train_loss', loss_value_reduce, epoch_1000x)
+        #     log_writer.add_scalar('lr', lr, epoch_1000x)
+    
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    
+    if log_writer is not None:
+        for k, v in metric_logger.meters.items():
+            if k in ['lr']:
+                log_writer.writer.add_scalar(proxy_single_client +'/opt/'+ k, v.global_avg, log_writer.step)  
+            elif k in ['loss']:
+                log_writer.writer.add_scalar(proxy_single_client +'/loss/'+ k, v.global_avg, log_writer.step) 
+            
+#             log_writer.update(loss=loss_value, head=proxy_single_client + "/loss")
+#             log_writer.update(loss_scale=loss_scale_value, head=proxy_single_client + "/opt")
+#             log_writer.update(lr=max_lr, head=proxy_single_client + "/opt")
+#             log_writer.update(min_lr=min_lr, head=proxy_single_client + "/opt")
+#             log_writer.update(weight_decay=weight_decay_value, head=proxy_single_client + "/opt")
+#             log_writer.update(grad_norm=grad_norm, head=proxy_single_client + "/opt")
+            
+            log_writer.set_step()
+#             print('step: ', self.step)
+#             print('global_step_per_client: ', args.global_step_per_client[proxy_single_client])
+    
+    args.current_mlm_acc[cur_single_client] = metric_logger.get_mlm_acc()
+    if args.best_mlm_acc[cur_single_client] < args.current_mlm_acc[cur_single_client]:
+        args.best_mlm_acc[cur_single_client] = args.current_mlm_acc[cur_single_client]
+    
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
---
+++ unilm/mae/script/retina_dis/retina_split1_imnet.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+cd /home/yan/SSL-FL/unilm/beit/
+
+DATASET='Retina'
+SPLIT_TYPE='split_1'
+N_CLASSES=2
+DATA_PATH="/data/yan/SSL-FL/${DATASET}/"
+N_CLIENTS=5
+
+# ------------------ finetune ----------------- #
+FT_EPOCHS=100
+FT_LR='2e-3'
+FT_BATCH_SIZE=64
+OUTPUT_PATH_FT="/data/yan/SSL-FL/fedavg_model_ckpt_${N_CLIENTS}/imnet_pretrained_beit_base/finetune_${DATASET}_epoch${FT_EPOCHS}_${SPLIT_TYPE}_lr${FT_LR}_bs${FT_BATCH_SIZE}"
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=4 run_class_finetuning_FedAvg_distributed.py \
+     --data_path ${DATA_PATH} \
+     --data_set ${DATASET} \
+     --finetune https://unilm.blob.core.windows.net/beit/beit_base_patch16_224_pt22k.pth \
+     --nb_classes ${N_CLASSES} \
+     --output_dir ${OUTPUT_PATH_FT} \
+     --lr ${FT_LR} \
+     --save_ckpt_freq 50 \
+     --model beit_base_patch16_224 \
+     --batch_size ${FT_BATCH_SIZE} --update_freq 1 --split_type ${SPLIT_TYPE} \
+     --warmup_epochs 5 --layer_decay 0.65 --drop_path 0.2 \
+     --weight_decay 0.05 --layer_scale_init_value 0.1 --clip_grad 3.0 \
+     --n_clients ${N_CLIENTS} --E_epoch 1 --max_communication_rounds ${FT_EPOCHS} --num_local_clients -1 
+
+# # ------------------ evaluate ----------------- #
+# CKPT_PATH="${OUTPUT_PATH_FT}/checkpoint-best.pth"
+# CUDA_VISIBLE_DEVICES=0 python run_class_finetuning_FedAvg_distributed.py \
+#     --eval --model beit_base_patch16_224 --data_path $DATA_PATH \
+#     --nb_classes ${N_CLASSES} --data_set ${DATASET} \
+#     --resume $CKPT_PATH \
+#     --batch_size ${BATCH_SIZE} \
+#     --E_epoch 1 --max_communication_rounds 100 --num_local_clients -1 --split_type ${SPLIT_TYPE}
+
+
+# ------------------ finetune ----------------- #
+# EPOCHS=100
+# LR='5e-4'
+# OUTPUT_PATH="/data/yan/SSL-FL/fedavg_model_ckpt/imnet_pretrained_beit_base/finetune_${DATASET}_epoch${EPOCHS}_${SPLIT_TYPE}_lr${LR}"
+
+# CUDA_VISIBLE_DEVICES=3 python run_class_finetuning_FedAvg.py \
+#      --data_path ${DATA_PATH} \
+#      --data_set ${DATASET} \
+#      --finetune https://unilm.blob.core.windows.net/beit/beit_base_patch16_224_pt22k.pth \
+#      --nb_classes ${N_CLASSES} \
+#      --output_dir ${OUTPUT_PATH} \
+#      --lr ${LR} \
+#      --save_ckpt_freq 20 \
+#      --model beit_base_patch16_224 \
+#      --batch_size 64  --update_freq 1 --split_type ${SPLIT_TYPE} \
+#      --warmup_epochs 5 --layer_decay 0.65 --drop_path 0.2 \
+#      --weight_decay 0.05 --layer_scale_init_value 0.1 --clip_grad 3.0 \
+#      --E_epoch 1 --max_communication_rounds ${EPOCHS} --num_local_clients -1 
+
+# # ------------------ evaluate ----------------- #
+# CKPT_PATH="${OUTPUT_PATH}/checkpoint-best.pth"
+# CUDA_VISIBLE_DEVICES=3 python run_class_finetuning_FedAvg.py \
+#     --eval --model beit_base_patch16_224 --data_path $DATA_PATH \
+#     --nb_classes ${N_CLASSES} --data_set ${DATASET} \
+#     --resume $CKPT_PATH \
+#     --batch_size 64 \
+#     --E_epoch 1 --max_communication_rounds 100 --num_local_clients -1 --split_type ${SPLIT_TYPE}
+
+EPOCHS=100
+LR='5e-4'
+OUTPUT_PATH="/data/yan/SSL-FL/fedavg_model_ckpt/imnet_pretrained_beit_base/finetune_${DATASET}_epoch${EPOCHS}_${SPLIT_TYPE}_lr${LR}"
+
+OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=16 python run_class_finetuning_FedAvg.py \
+     --data_path ${DATA_PATH} \
+     --data_set ${DATASET} \
+     --finetune https://unilm.blob.core.windows.net/beit/beit_base_patch16_224_pt22k.pth \
+     --nb_classes ${N_CLASSES} \
+     --output_dir ${OUTPUT_PATH} \
+     --lr ${LR} \
+     --save_ckpt_freq 20 \
+     --model beit_base_patch16_224 \
+     --batch_size 64  --update_freq 1 --split_type ${SPLIT_TYPE} \
+     --warmup_epochs 5 --layer_decay 0.65 --drop_path 0.2 \
+     --weight_decay 0.05 --layer_scale_init_value 0.1 --clip_grad 3.0 \
+     --E_epoch 1 --max_communication_rounds ${EPOCHS} --num_local_clients -1 
+
+# ------------------ evaluate ----------------- #
+# CKPT_PATH="${OUTPUT_PATH}/checkpoint-best.pth"
+# CUDA_VISIBLE_DEVICES=3 python run_class_finetuning_FedAvg.py \
+#     --eval --model beit_base_patch16_224 --data_path $DATA_PATH \
+#     --nb_classes ${N_CLASSES} --data_set ${DATASET} \
+#     --resume $CKPT_PATH \
+#     --batch_size 64 \
+#     --E_epoch 1 --max_communication_rounds 100 --num_local_clients -1 --split_type ${SPLIT_TYPE}
